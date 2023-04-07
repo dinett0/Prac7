94ea8c34b257d11536666b166623247a01fe0c7a
David <chuckbucker94@gmail.com>
2023-04-08 00:03:47 +0300

    MVVM pattern, DataSource classes, LiveData

--- app/src/main/java/com/example/prac5_r/Data_layer/Repository.java
+++ app/src/main/java/com/example/prac5_r/Data_layer/Repository.java
@@ -1,6 +1,33 @@
 package com.example.prac5_r.Data_layer;
 
+import com.example.prac5_r.Data_layer.DataSource.Custom;
+import com.example.prac5_r.Data_layer.DataSource.User;
 
 public class Repository {
+    private User client;
+    private Custom item;
 
+    public Repository(User client) {
+        this.client = client;
+    }
+
+    public Repository(Custom item) {
+        this.item = item;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Custom getItem() {
+        return item;
+    }
+
+    public void setItem(Custom item) {
+        this.item = item;
+    }
 }
